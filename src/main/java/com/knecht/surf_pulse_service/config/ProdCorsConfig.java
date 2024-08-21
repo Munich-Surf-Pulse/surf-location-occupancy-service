59f173a23062d363837d7dec03a83fc7110254f4
Gerald Mahlknecht <>
2024-08-21 09:30:48 +0200

    add cors for prod

---
+++ src/main/java/com/knecht/surf_pulse_service/config/ProdCorsConfig.java
@@ -0,0 +1,30 @@
+package com.knecht.surf_pulse_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("prod")
+public class ProdCorsConfig {
+
+    @Value("${FRONTEND_DOMAIN}")
+    private String frontendDomain;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendDomain)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
